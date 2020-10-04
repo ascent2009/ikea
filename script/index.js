d82161d1c1609685c6d211f31d9163e7c6bb489d
ascent2009 <ascent2009@hotmail.com>
2020-10-04 14:12:35 +0300

    day 5 homework: add cart page render (unsucsessful)

--- script/index.js
+++ script/index.js
@@ -4,15 +4,12 @@ import generateHeader from "./generateHeader.js";
 import generateFooter from "./generateFooter.js";
 import generateCatalog from "./generateCatalog.js";
 import generateGoodsPage from "./generateGoodsPage.js";
-import { loadData } from "./loadData.js";
+import generateCartPage from "./generateCartPage.js";
 import generateItemPage from "./generateItemPage.js";
-// import storage from "./storage.js";
 
 generateHeader();
 generateCatalog();
 generateGoodsPage();
 generateItemPage();
-// storage();
+generateCartPage;
 generateFooter();
-
-loadData();
