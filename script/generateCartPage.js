a9b9fd35fda1a2bd2e8199c129cb991981039a1d
ascent2009 <ascent2009@hotmail.com>
2020-10-05 21:24:33 +0300

    day 6 homework: add fetch, get/send data, send form data

--- script/generateCartPage.js
+++ script/generateCartPage.js
@@ -1,6 +1,45 @@
 import { getData } from "./getData.js";
 import userData from "./userData.js";
 
+const sendData = async (url, data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    body: data,
+  });
+  if (!response.ok) {
+    throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response}`);
+  }
+  return await response.json();
+};
+
+const sendCart = () => {
+  const cartForm = document.querySelector(".cart-form");
+
+  const data = {
+    name: "Плюшевый Мишка",
+    count: 3,
+  };
+
+  cartForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(cartForm);
+
+    const data = {};
+    for (const [key, value] of formData) {
+      data[key] = value;
+    }
+
+    data.order = userData.cartList;
+
+    console.log(data);
+    sendData("https://jsonplaceholder.typicode.com/posts", JSON.stringify(data))
+      .then(() => {
+        cartForm.reset();
+      })
+      .catch((err) => console.error(err));
+  });
+};
+
 const generateCartPage = () => {
   if (location.pathname.includes("cart")) {
     const cartList = document.querySelector(".cart-list");
@@ -103,6 +142,7 @@ const generateCartPage = () => {
     });
 
     getData.cart(userData.cartList, renderCartList);
+    sendCart();
   }
 };
 
